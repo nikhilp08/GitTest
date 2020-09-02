2902e3bb8d2d96305575e81ad9adfa114ef9cfee
Nikhil Patel <np02292008@gmail.com>
2020-09-02 12:36:01 -0400

    ground

--- sketch.js
+++ sketch.js
@@ -15,8 +15,8 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
-    ground = new Ground(600,height,1200,20);
+    
+    //ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
